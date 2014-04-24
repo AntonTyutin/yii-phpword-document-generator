ca3424eadb7d6d3058c2bb83c88ceb882f9e84d2
Vadim Sannikov <vadim.sannikov.work@gmail.com>
2014-04-25 02:58:48 +0400

    Support for arrays as replacement

--- src/PhpWordRenderer.php
+++ src/PhpWordRenderer.php
@@ -25,10 +25,115 @@ class PhpWordRenderer implements RendererInterface
     public function render($templatePath, array $data)
     {
         $doc = new Template($templatePath);
+
+        $property = new \ReflectionProperty($doc, '_documentXML');
+        $property->setAccessible(true);
+        $xml = $property->getValue($doc);
+
         foreach ($data as $placeholder => $value) {
-            $doc->setValue($placeholder, $value);
+
+            if (false !== strpos($xml, $placeholder)) {
+                list($placeholder, $value) = $this->prepareBeforeSaveValue($xml, $placeholder, $value);
+                $property->setValue($doc, $xml);
+
+                foreach ($placeholder as $key => $search) {
+                    $doc->setValue($search, $value[$key]);
+                }
+
+                $xml = $property->getValue($doc);
+            }
         }
+
         return $this->getTemporaryFileContents($doc->save());
+    }
+
+    /**
+     * prepareBeforeSaveValue
+     *
+     * @param string $documentXML
+     * @param mixed  $search
+     * @param mixed  $replace
+     *
+     * @return array
+     */
+    protected function prepareBeforeSaveValue(&$documentXML, $search, $replace)
+    {
+        $result  = [[], []];
+        $replace = (array) $replace;
+
+        foreach ((array) $search as $keyS => $valueS) {
+            if (isset($replace[$keyS])) {
+                if (is_array($replace[$keyS])) {
+                    $dom = new \DomDocument();
+                    $dom->loadXML($documentXML);
+                    $list = $this->getWPNodesList($dom, $valueS);
+
+                    foreach ($replace[$keyS] as $keyR => $valueR) {
+                        $result[0][] = ($newValueS = $this->getEnclosedSearchParam($valueS . '_' . $keyR));
+                        $result[1][] = $valueR;
+
+                        foreach ($list as $node) {
+                            $newNode = $node->parentNode->insertBefore($node->cloneNode(true), $node);
+
+                            foreach ($this->getWPNodesList($dom, $valueS, false, $newNode) as $innerNode) {
+                                $innerNode->nodeValue = $newValueS;
+                            }
+                        }
+                    }
+
+                    foreach ($this->getWPNodesList($dom, $valueS) as $node) {
+                        $node->parentNode->removeChild($node);
+                    }
+
+                    $documentXML = $dom->saveXML();
+
+                } else {
+                    $result[0][] = $this->getEnclosedSearchParam($valueS);
+                    $result[1][] = $replace[$keyS];
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Search for parent "<w:p>" tag
+     *
+     * @param \DomDocument $dom
+     * @param string       $search
+     * @param boolean      $isParent
+     * @param \DOMNode     $node
+     *
+     * @return \DOMNodeList
+     */
+    protected function getWPNodesList(\DomDocument $dom, $search, $isParent = true, $node = null)
+    {
+        $list = (new \DomXPath($dom))->query(
+            (isset($node) ? '.' : '')
+                . '//w:t[.="' . $this->getEnclosedSearchParam($search) . '"]'
+                . ($isParent ? '/../..' : ''),
+            $node
+        );
+
+        return $list instanceof \DOMNodeList ? $list : new \DOMNodeList();
+    }
+
+    /**
+     * getEnclosedSearchParam
+     *
+     * @param string  $value
+     * @param boolean $check
+     *
+     * @return string
+     */
+    protected function getEnclosedSearchParam($value, $check = true)
+    {
+        if (!$check || !preg_match('/^\$\{\w+\}$/S', $value)) {
+            $value = '${' . $value . '}';
+        }
+
+        return $value;
     }
 
     /**
